804a2375ef1b78109f42337404bdccc4fe1ac598
sanasiyal <160250355+sanasiyal@users.noreply.github.com>
2024-07-13 09:44:39 -0700

    Delete Q.no 3 directory

--- Q.no 3/main.ts
+++
@@ -1,7 +0,0 @@
-// Q.no 3....Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-
-
-let firstName:string = "sana siyal";
-console.log(firstName.toLowerCase());
-console.log(firstName.toUpperCase());
-console.log(firstName.charAt(0).toUpperCase() + firstName.slice(5));
