c4d3dc53c21e60963c5d60022b2f85bc00b252f8
sanasiyal <160250355+sanasiyal@users.noreply.github.com>
2024-07-13 09:26:24 -0700

    Add files via upload

---
+++ Q.no 10/main.ts
@@ -0,0 +1,10 @@
+// Qno 10....Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+// author: [sana siyal]
+//date:[wednesday 22 may 2024]
+
+//this is my favourit number
+let myFvrtNumber = 44
+
+//reveling my favourit number
+console.log(`my favourite number is ${myFvrtNumber}`);
---
+++ Q.no 4/qno.4.js
@@ -0,0 +1,5 @@
+"use strict";
+// QNO.4...Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation mark
+let quote = "“A person who never made a mistake never tried anything new.”";
+let author = "Albert Einstein once said,";
+console.log(author + quote);
