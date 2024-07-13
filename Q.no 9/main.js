a6a40cc6ed6d2d177a07ca6a6aa4603b5713ad0e
sanasiyal <160250355+sanasiyal@users.noreply.github.com>
2024-07-13 09:46:19 -0700

    Delete Q.no 9 directory

--- Q.no 9/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// Q.no 9.. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
-let num = 4235;
-console.log(`My favourite number is ${num}`);
--- Q.no 9/main.ts
+++
@@ -1,4 +0,0 @@
-// Q.no 9.. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
-
-let num: number = 4235;
-console.log(`My favourite number is ${num}`);
