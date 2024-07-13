d7d411699d3949aa096b3b7c8462a75cd01d95a4
sanasiyal <160250355+sanasiyal@users.noreply.github.com>
2024-07-13 09:27:31 -0700

    Delete Q.no 11 directory

--- Q.no 11/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
-let names = ["shifa", "manahil", " zara", "rimsha"];
-console.log((names[0]));
-console.log((names[1]));
-console.log((names[2]));
-console.log((names[3]));
