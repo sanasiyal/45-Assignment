e5befc01abaea838e5e85c28bbf65dbae78026b6
sanasiyal <160250355+sanasiyal@users.noreply.github.com>
2024-07-13 09:45:34 -0700

    Delete Q.no 5 directory

--- Q.no 5/main.ts
+++
@@ -1,7 +0,0 @@
-// Q.no 5...Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
-
-let famous_person:string = "hazrat ali";
-
-let message:string = "accept the apology, even if it is not sincer";
-
-console.log(`${famous_person} once said, "${message}"`);
