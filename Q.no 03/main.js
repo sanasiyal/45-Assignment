fd07ac19a6f29858afdc28316e6a7427a09585f9
sanasiyal <160250355+sanasiyal@users.noreply.github.com>
2024-07-13 09:47:53 -0700

    Add files via upload

---
+++ Q.no 03/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Q.no 3....Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let firstName = "sana siyal";
+console.log(firstName.toLowerCase());
+console.log(firstName.toUpperCase());
+console.log(firstName.charAt(0).toUpperCase() + firstName.slice(5));
---
+++ Q.no 06/main.ts
@@ -0,0 +1,6 @@
+// Q.no 6....Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+
+
+console.log("sana\tSiyal")
+console.log("sana\nSiyal");
+
