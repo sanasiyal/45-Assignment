18ab9408aa5bc63e0bae60a021bd48f32f4d6ba6
sanasiyal <160250355+sanasiyal@users.noreply.github.com>
2024-07-13 09:44:58 -0700

    Delete Q.no 4 directory

--- Q.no 4/qno.4.ts
+++
@@ -1,6 +0,0 @@
-// QNO.4...Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation mark
-
-let quote:string = "“A person who never made a mistake never tried anything new.”";
-let author:string = "Albert Einstein once said,";
-console.log(author + quote);
-
